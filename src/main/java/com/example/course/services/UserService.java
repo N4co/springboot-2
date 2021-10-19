9e8f2490730abbf4e90ab368c1950709a031c457
n4co <nf.oliveira@hotmail.com>
2021-10-19 13:53:44 -0300

    Exception Handling Delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -43,12 +43,15 @@ public class UserService {
 	}
 	}
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity,obj);
 		return repository.save(entity);
-		
+		}catch(RuntimeException e ) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
-
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
