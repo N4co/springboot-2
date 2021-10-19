48b3c2d87c5a7dc2df0231826cb0433f684c9dfc
n4co <nf.oliveira@hotmail.com>
2021-10-19 02:03:56 -0300

    User Delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
@@ -27,6 +28,11 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repository.save(obj);
-		
 	}
+
+	public void delete(Long id) {
+	repository.deleteById(id);
+	}
+
+
 }
