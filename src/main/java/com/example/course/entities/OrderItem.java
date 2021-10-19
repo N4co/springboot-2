b3b4adae526ec612d6efbdfadef23bdfa18e24bd
n4co <nf.oliveira@hotmail.com>
2021-10-19 00:21:58 -0300

    Order ITem parte 2

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -7,7 +7,8 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import com.example.course.entities.pk.OrdemItemPK;
+import com.example.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 
    @EmbeddedId
-	private OrdemItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	
 	private Integer quantity;
@@ -33,6 +34,8 @@ public class OrderItem implements Serializable{
 		this.quantity = quantity;
 		this.price = price;
 	}
+	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
--- src/main/java/com/example/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/example/course/entities/pk/OrderItemPK.java
@@ -11,7 +11,7 @@ import com.example.course.entities.Order;
 import com.example.course.entities.Product;
 
 @Embeddable
-public class OrdemItemPK implements Serializable {
+public class OrderItemPK implements Serializable {
    private static final long serialVersionUID = 1L;
 	
    @ManyToOne
@@ -45,7 +45,7 @@ public class OrdemItemPK implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		OrdemItemPK other = (OrdemItemPK) obj;
+		OrderItemPK other = (OrderItemPK) obj;
 		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
 	}
 	
